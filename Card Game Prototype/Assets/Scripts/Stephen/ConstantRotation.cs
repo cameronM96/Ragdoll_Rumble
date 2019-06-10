b8f74dadd8034075f1e2d56b224e938ddde4ca34
Stephen Cav <1011091@student.sae.edu.au>
2019-06-11 00:24:54 +1000

    Created mock up of main menu/collection/campaign/options tidied up my files

---
+++ Card Game Prototype/Assets/Scripts/Stephen/ConstantRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour { 
+
+    public GameObject target;
+    public float RotationSpeed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        target.transform.Rotate(new Vector3(0, Time.deltaTime, 0) * RotationSpeed);
+    }
+}
