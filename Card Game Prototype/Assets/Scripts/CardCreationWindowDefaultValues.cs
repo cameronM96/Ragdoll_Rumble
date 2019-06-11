e640021a567cc3fbed95d6e6dd70a8aa50d65a24
cameron.mullins@hotmail.fr <Hubblerocks1!>
2019-06-11 19:09:58 +1000

    Card Template

--- Card Game Prototype/Assets/Scripts/CardCreationWindowDefaultValues.cs
+++ Card Game Prototype/Assets/Scripts/CardCreationWindowDefaultValues.cs
@@ -6,5 +6,6 @@ public class CardCreationWindowDefaultValues : MonoBehaviour
 {
     public GameObject cardTemplate;
     public Sprite[] backgrounds;
+    public Sprite[] rarity;
     public RenderTexture previewWindowTexture;
 }
