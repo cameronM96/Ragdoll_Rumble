bb31898674b8cc4a8a079f1e2a226a6889537837
StephenSAE <37386605+StephenSAE@users.noreply.github.com>
2019-06-25 17:53:22 +1000

    Pushing level edits to Dojo and added models from Ivy

---
+++ Card Game Prototype/Assets/Editor/LoadSceneEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LoadQuickScene))]
+public class LoadSceneEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LoadQuickScene loadScene = target as LoadQuickScene;
+
+        loadScene.sceneNameCheck = EditorGUILayout.Toggle("Use Scene Name", loadScene.sceneNameCheck);
+
+        if (loadScene.sceneNameCheck)
+            loadScene.sceneName = EditorGUILayout.TextField(loadScene.sceneName);
+        else
+            loadScene.sceneBuildNumber = EditorGUILayout.IntField(loadScene.sceneBuildNumber);
+    }
+}
---
+++ Card Game Prototype/Assets/Scripts/Stephen/ObseleteSceneScripts/LoadQuickScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadQuickScene : MonoBehaviour
+{
+    public bool sceneNameCheck;
+    public int sceneBuildNumber;
+    public string sceneName;
+
+    public void LoadScene()
+    {
+        if (sceneNameCheck)
+            SceneManager.LoadScene(sceneName);
+        else
+            SceneManager.LoadScene(sceneBuildNumber);
+    }
+}
