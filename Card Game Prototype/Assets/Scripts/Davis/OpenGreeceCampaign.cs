9d624cdff3a1aeaae8f43c0b3cb84cd2b24cd09c
Cameron Davis <37386600+cdavis3108@users.noreply.github.com>
2019-07-11 22:14:41 +1000

    Finished Campaign Level Select

---
+++ Card Game Prototype/Assets/Scripts/Davis/OpenGreeceCampaign.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenGreeceCampaign : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("GreeceCampaign");
+    }
+}
