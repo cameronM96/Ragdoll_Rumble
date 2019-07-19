02363b321862bfa4945411b04374bb0969e71b0d
Stephen Cav <1011091@student.sae.edu.au>
2019-07-20 01:35:16 +1000

    Fixed Main menu scaling issues/added new menu font/added animation script to title (Nearly finished custom buttons and backings

---
+++ Card Game Prototype/Assets/TextAnimSequencer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAnimSequencer : MonoBehaviour
+{
+    List<Animator> _animators;
+    void Start()
+    {
+        _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+
+        StartCoroutine(DoAnim());
+    }
+
+    IEnumerator DoAnim() {
+        while (true)
+        {
+            foreach (var animator in _animators)
+            {
+                animator.SetTrigger("Animate");
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+    }
+}
