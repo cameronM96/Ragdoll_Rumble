c51309fcefd512f311124ca020fecc529f15829d
StephenSAE <37386605+StephenSAE@users.noreply.github.com>
2019-07-30 19:14:47 +1000

    Lobby screen changes

--- Card Game Prototype/Assets/Scripts/Card Collection/PanelResizer.cs
+++ Card Game Prototype/Assets/Scripts/Card Collection/PanelResizer.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class PanelResizer : MonoBehaviour
 {
     public RectTransform myRect;
+    public bool width = false;
+    public bool height = true;
 
     private void Start()
     {
@@ -15,16 +17,35 @@ public class PanelResizer : MonoBehaviour
     public void Resize()
     {
         float targetHeight = 0;
+        float targetWidth = 0;
 
-        // Get size panel needs to be based on children
-        GridLayoutGroup gLayout = GetComponent<GridLayoutGroup>();
-        targetHeight = gLayout.padding.top;
-        targetHeight += (gLayout.cellSize.y + gLayout.spacing.y) * (transform.childCount / gLayout.constraintCount);
+        if (width)
+        {
+            // Get size panel needs to be based on children
+            GridLayoutGroup gLayout = GetComponent<GridLayoutGroup>();
+            targetWidth = gLayout.padding.left;
+            targetWidth += (gLayout.cellSize.x + gLayout.spacing.x) * (transform.childCount / gLayout.constraintCount);
 
-        if (transform.childCount % gLayout.constraintCount != 0)
-            targetHeight += (gLayout.cellSize.y + gLayout.spacing.y);
+            if (transform.childCount % gLayout.constraintCount != 0)
+                targetWidth += (gLayout.cellSize.x + gLayout.spacing.x);
 
-        // Set size
-        myRect.sizeDelta = new Vector2(0, targetHeight);
+            // Set size
+            myRect.sizeDelta = new Vector2(targetWidth, 0);
+        }
+
+        if (height)
+        {
+            // Get size panel needs to be based on children
+            GridLayoutGroup gLayout = GetComponent<GridLayoutGroup>();
+            targetHeight = gLayout.padding.top;
+            targetHeight += (gLayout.cellSize.y + gLayout.spacing.y) * (transform.childCount / gLayout.constraintCount);
+
+            if (transform.childCount % gLayout.constraintCount != 0)
+                targetHeight += (gLayout.cellSize.y + gLayout.spacing.y);
+
+            // Set size
+            myRect.sizeDelta = new Vector2(0, targetHeight);
+        }
+        
     }
 }
