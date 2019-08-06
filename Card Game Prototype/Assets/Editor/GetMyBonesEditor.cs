48ec418eadc2afafe986d11f9ea008e2122ee481
Davis <37386600+cdavis3108@users.noreply.github.com>
2019-08-06 19:11:31 +1000

    Improved animation and bone parent script

---
+++ Card Game Prototype/Assets/Editor/GetMyBonesEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(GetMeParent))]
+public class GetMyBonesEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GetMeParent setbonesscript = target as GetMeParent;
+
+        if(GUILayout.Button("Set ME MA BNONEZ"))
+        {
+            setbonesscript.SetMyBones();
+            Debug.Log("Settings Bonezzsdf");
+        }
+    }
+}
---
+++ Card Game Prototype/Assets/Scripts/Davis/GetMeParent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMeParent : MonoBehaviour
+{
+    public void SetMyBones()
+    {
+        GameObject parent = transform.parent.gameObject;
+        HingeJoint joint = GetComponent<HingeJoint>();
+        joint.connectedBody = parent.GetComponent<Rigidbody>();
+    }
+}
