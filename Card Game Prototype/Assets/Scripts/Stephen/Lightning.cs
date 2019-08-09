174f367efe4443fa049fec3bdf883d77df68eaca
Stephen Cav <1011091@student.sae.edu.au>
2019-08-10 01:59:26 +1000

    Dojo rain and lightning 1

---
+++ Card Game Prototype/Assets/Scripts/Stephen/Lightning.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+    public float minTime;
+    public float threshhold;
+    public float LastTime;
+    public GameObject lightning1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightning1.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Time.time - LastTime) > minTime)
+        {
+            if(Random.value > threshhold)
+            {
+                lightning1.SetActive (true);
+                Debug.Log("hdjahsd");
+            }
+            else
+            {
+                lightning1.SetActive(false);
+                LastTime = Time.time;
+            }
+            
+        }
+    }
+}
